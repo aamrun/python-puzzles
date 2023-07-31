ecd4ac6bf1f368ac99bf64b6b85cc4445e0b3ccd
Abhishek Ghosh <abhishek.ghosh@igs-gmbh.ch>
2023-07-31 14:27:59 +0200

    Added implementation for Longest Substring Without Repeating Characters

---
+++ longest_substring_without_repeating_characters/implementation.py
@@ -0,0 +1,14 @@
+#Implementation of python-puzzles/longest_substring_without_repeating_characters
+
+#This is essentially a one liner solution utilising Python's list generator feature. 
+#The inner list generator generates the set of all consecutive substrings of the given string. 
+#This list is converted into a set to eliminate duplicates. 
+#The outer list generator works on this set and generates a list of lengths for each element of the set which has only unique characters. 
+#The max() function ultimately gives the desired result.
+
+class Solution:
+  def lengthOfLongestSubstring(self, string):
+    return max([len(set(element)) for element in set([string[y:2+x] for y in range(0,len(string)) for x in range(0,len(string[1:]))]) if len(element) == len(set(element))])
+
+print(Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx'))
+# 10
