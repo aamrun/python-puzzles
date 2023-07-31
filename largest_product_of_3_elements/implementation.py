8714ce29af79a2dba90361c08abd6f471f1950a6
Abhishek Ghosh <abhishek.ghosh@igs-gmbh.ch>
2023-07-31 11:38:50 +0200

    Added largest product of 3 elements

---
+++ largest_product_of_3_elements/implementation.py
@@ -0,0 +1,15 @@
+import itertools
+
+def maximum_product_of_three(lst):
+  maximum = None
+
+  for subset in itertools.combinations(lst,3):
+    product = subset[0] * subset[1] * subset[2]
+
+    if maximum is None or maximum < product:
+     maximum = product
+
+  return maximum
+
+print(maximum_product_of_three([-4, -4, 2, 8]))
+# 128
---
+++ largest_product_of_3_elements/readme.txt
@@ -0,0 +1,1 @@
+Given an array of integers, return the largest product that can be made by multiplying any 3 integers in the array.
