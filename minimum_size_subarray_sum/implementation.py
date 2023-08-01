61a2bbd267d764e352d9309c68a4b9a3fb6c026f
Abhishek Ghosh <abhishek.ghosh@igs-gmbh.ch>
2023-08-01 10:26:26 +0200

    Changed dictionary to list in minimum_size_subarray_sum/implementation.py

--- minimum_size_subarray_sum/implementation.py
+++ minimum_size_subarray_sum/implementation.py
@@ -2,14 +2,13 @@
 
 #This is another almost one liner solution utilising Python's list comprehension feature, similar to the implementation of python-puzzles/longest_palindromic_substring
 #The inner list comprehension generates the set of all non-empty subsets of the given number list.
-#This list is then used in a dictionary comprehension to form key value pairs of the subsets and their sums, provided those sums equal the required sum s.
-#Note that the dictionary comprehension casts or converts the subsets as strings. This is because Python lists are unhashable and cannot be used as dictionary keys. 
-#The workaround is to use strings which for all purposes are identical to character lists.
+#This list is then used in an outer list comprehension to store only those subsets whose sums equal the required sum s.
+#A previous version of this implementation used a dictionary to store key value pairs of the lists and their sums. Since all the sums are the same, storing it is not required.
 #The min() function ultimately gives the desired result using a lambda function.
 
 class Solution:
   def minSubArray(self, nums, s):
-    sum_set = {str(nums):sum(nums) for nums in [nums[y:2+x] for y in range(0,len(nums)) for x in range(0,len(nums[1:])) if len(nums[y:2+x])!=0] if sum(nums) == s}
+    sum_set = [nums for nums in [nums[y:2+x] for y in range(0,len(nums)) for x in range(0,len(nums[1:])) if len(nums[y:2+x])!=0] if sum(nums) == s]
     if sum_set is None or len(sum_set)==0:
      return None
     else:
